b6fe0418f5fae6685bbcdc3103e316870ea2e9a5
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-05 20:48:39 -0800

    Refactor server.js and do a bit of housekeeping

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 //server.js
+'use strict';
 
-var app = require('./app');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const weatherRouter = require('./routers/v1/weather-router');
+const { PORT } = require('./config');
 
-var port = process.env.PORT || 3000;
+const app = express();
 
-var server = app.listen(port, function () {
-	console.log('Express server is running on port ' + port);
-})
+app.use(morgan('common'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); 
+
+app.use('/api/v1', weatherRouter);
+
+
+app.use(function(req, res, next){
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {}
+  });
+});
+
+var server = app.listen(PORT, function () {
+	console.log('Express server is running on port ' + PORT);
+}).on('error', err => console.error(err));
